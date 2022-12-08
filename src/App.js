3517d13a4d518a620e6776d384917e250a816576
LauraS <laura.silvani@sicpa.com>
2022-12-08 10:18:34 +0100

    core: light and dark mode experimentation with CSS custom vars in a SCSS env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './scss/main.scss';
-import { Card } from './components/card/Card';
+import './css/main.scss';
+import { Button } from './components/button/Button';
 import React from 'react';
 
 function App() {
@@ -7,10 +7,18 @@ function App() {
   return (
     <React.Fragment>
       {/* Add  the class to the main container */} 
-      <div className="theme-sds-light">
+      <div>
         <main>
           <section>
-            <Card></Card>
+          <article>
+            <div><img src="https://cdn.dribbble.com/users/224254/avatars/normal/0dc7fb7cdce4ef573f09ae3e20681610.jpg?1489055809" alt="Laura Silvani"/></div>
+            <div>
+              <h1>Laura Silvani</h1>
+              <a href="http://www.google.it">@lsilvani</a>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <Button>Get in touch</Button>
+          </div>
+      </article>
           </section> 
         </main>
     </div>
